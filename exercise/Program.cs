53bee41a83cddd2d6b613b4ec198f0c5acb83df9
Atiyeh <mhmdalatiyeh@gmail.com>
2025-10-06 13:22:20 +0300

    committing Product class

--- exercise/Program.cs
+++ exercise/Program.cs
@@ -10,6 +10,26 @@ namespace exercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to The Inventory Console");
         }
+        public class Product
+        {
+            public string productName { get; set; }
+            public double productPrice { get; set; }
+            public int productQuantity { get; set; }
+
+            public Product(string productName, double productPrice, int productQuantity)
+            {
+                this.productName = productName;
+                this.productPrice = productPrice;
+                this.productQuantity = productQuantity;
+            }
+
+            public override string ToString()
+            {
+                return $"Product name : {productName} --- Product price : {productPrice} --- Product quantity : {productQuantity}";
+            }
+        }
+
     }
 }
